4dbd1128dee8b52fc6fa79b7de03ee43bcd6616c
Arthur Prydatko <artyrpridatko@gmail.com>
2026-06-04 16:59:40 +0300

    feat(CI): change playwright config and ci for html report

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -4,9 +4,13 @@ export default defineConfig({
   testDir: "./e2e",
   globalSetup: "./e2e/global-setup.ts",
   globalTeardown: "./e2e/global-teardown.ts",
+  reporter: [["list"], ["html", { open: "never" }]],
+  retries: process.env.CI ? 2 : 0,
   use: {
     baseURL: "http://127.0.0.1:3000",
+    screenshot: "only-on-failure",
     trace: "on-first-retry",
+    video: "retain-on-failure",
   },
   webServer: {
     command: "pnpm start",
