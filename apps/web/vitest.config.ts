78d4ac49dae6b6839d91d77f08b480df12f2e14d
Arthur Prydatko <artyrpridatko@gmail.com>
2026-06-04 17:13:01 +0300

    feat(CI): add coverage summary

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
     },
   },
   test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary"],
+    },
     environment: "jsdom",
     exclude: ["**/e2e/**", "**/node_modules/**", "**/.git/**"],
     outputFile: {
